7013f50c58ad4edca28178e2d1758bf79313bd18
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-05-08 10:16:29 -0500

    Connect actions and reducers

--- src/reducers/reducer-books.js
+++ src/reducers/reducer-books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'JavaScript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'Eloquent Ruby' },
-    { title: 'The Dark Tower' },
+    { title: 'JavaScript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 38 },
+    { title: 'Eloquent Ruby', pages: 1 },
+    { title: 'The Dark Tower', pages: 300 },
   ];
 }
